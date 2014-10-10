3f810e0c81ed517c9fe81a621943f52f2118e45e
Tim Collins <tim@timcollins.ie>
2014-10-10 13:04:40 +0100

    Initial solution structure and attempt at solving Challenge #183 [Easy] Semantic Version Sort - http://www.reddit.com/r/dailyprogrammer/comments/2igfj9/10062014_challenge_183_easy_semantic_version_sort/

---
+++ src/SemanticVersionSort/SemanticVersionEntity.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace SemanticVersionSort
+{
+    class SemanticVersionEntity
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Patch { get; set; }
+        public string Label { get; set; }
+        public string MetaData { get; set; }
+
+        public static bool operator <(SemanticVersionEntity first, SemanticVersionEntity second)
+        {
+            // Sorting rules
+            //1. First, compare the major version.
+            //2. If they are the same, compare the minor version.
+            //3. If they are the same, compare the patch version.
+            //3. If those are all the same, check if the version has a label - ignore the 
+            // content of the label. A version with a label (prerelease) comes before one 
+            // without a label (final release.)
+            //5. Ignore the build metadata.
+
+            if (first.Major < second.Major)
+            {
+                return true;
+            }
+
+            if (first.Minor < second.Minor)
+            {
+                return true;
+            }
+
+            if (first.Patch < second.Patch)
+            {
+                return true;
+            }
+
+            return !string.IsNullOrEmpty(first.Label);
+        }
+
+        public static bool operator >(SemanticVersionEntity first, SemanticVersionEntity second)
+        {
+            if (first.Major > second.Major)
+            {
+                return true;
+            }
+
+            if (first.Minor > second.Minor)
+            {
+                return true;
+            }
+
+            if (first.Patch > second.Patch)
+            {
+                return true;
+            }
+
+            return string.IsNullOrEmpty(first.Label);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append(string.Format("{0}.{1}.{2}", Major, Minor, Patch));
+            
+            if (!string.IsNullOrEmpty(Label))
+            {
+                output.Append(string.Format("-{0}", Label));
+            }
+
+            if (!string.IsNullOrEmpty(MetaData))
+            {
+                output.Append(string.Format("+{0}", MetaData));
+            }
+
+            return output.ToString();
+        }
+    }
+}

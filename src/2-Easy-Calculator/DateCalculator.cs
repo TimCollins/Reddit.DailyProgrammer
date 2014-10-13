949481b6806cadaa9f0aa638fc7882e5ba053f8a
Tim Collins <tim@timcollins.ie>
2014-10-13 15:32:56 +0100

    Added initial Calculator project with some unit tests for input validation.

---
+++ src/2-Easy-Calculator/DateCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_Easy_Calculator
+{
+    public static class DateCalculator
+    {
+        public static DateTime GetDateFromString(string date)
+        {
+            string[] parts = date.Split('/');
+
+            if (parts.Length != 3)
+            {
+                throw new ArgumentException("date");
+            }
+
+            int year;
+            int month;
+            int day;
+
+            if (!Int32.TryParse(parts[2], out year))
+            {
+                throw new ArgumentException("date");
+            }
+
+            if (!Int32.TryParse(parts[1], out month))
+            {
+                throw new ArgumentException("date");
+            }
+
+            if (!Int32.TryParse(parts[0], out day))
+            {
+                throw new ArgumentException("date");
+            }
+
+            if (year < 1 || month < 1 || day < 1)
+            {
+                throw new ArgumentException("date");
+            }
+
+            //ArgumentException
+            // IndexOutOfRangeException
+            // ArgumentOutofRangeException
+            return new DateTime(Convert.ToInt32(parts[2]), Convert.ToInt32(parts[1]), Convert.ToInt32(parts[0]));
+        }
+
+        public static int GetWorkingDays(DateTime startDate, DateTime endDate)
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/2-Easy-Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Util;
+
+namespace _2_Easy_Calculator
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pjbj8/easy_challenge_2/
+    /// ...Your challenge for today is to create a calculator application that has use in 
+    /// your life.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayStartText();
+
+            Console.Write("Enter start date in dd/mm/yyyy format: ");
+            string start = Console.ReadLine();
+
+            Console.Write("Enter end date in dd/mm/yyyy format: ");
+            string end = Console.ReadLine();
+
+            DateTime startDate = DateCalculator.GetDateFromString(start);
+            DateTime endDate = DateCalculator.GetDateFromString(end);
+
+            int workingDays = DateCalculator.GetWorkingDays(startDate, endDate);
+
+            Console.WriteLine("There are {0} working days between {1} and {2}.", workingDays, start, end);
+
+            ConsoleUtils.WaitForEscape();
+        }
+
+        private static void DisplayStartText()
+        {
+            Console.WriteLine("This program will calculate the number of working days between two given dates.");
+        }
+    }
+}

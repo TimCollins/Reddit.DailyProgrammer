f5dcc1a89e5e45b3d6eccd19982316140965bf66
Tim Collins <tim@timcollins.ie>
2014-10-13 14:31:01 +0100

    Code tidy-up.

--- src/1-Diff-Guesser/Program.cs
+++ src/1-Diff-Guesser/Program.cs
@@ -14,76 +14,34 @@ namespace _1_Diff_Guesser
 
             DoGuess(1, 100, 1);
 
-            ConsoleUtils.WaitForEscape();
-            return;
-
-
-            char input = ' ';
-            int guess;
-            int low = 0;
-            int high = 100;
-            int guesses = 0;
-
-            while (input != 'e')
-            {
-                guess = (low + high) / 2;
-                Console.WriteLine("Is your number (h)igher than, (l)ower than or (e)qual to {0}?", guess);
-                DisplayPrompt();
-
-                ConsoleKeyInfo cki = Console.ReadKey(true);
-                input = cki.KeyChar;
-
-                switch (input)
-                {
-                    case 'h':
-                        low = guess;
-                        break;
-                    case 'l':
-                        high = guess;
-                        break;
-                }
-
-                guesses++;
-            }
-
-            DisplayEndText(guesses);
-            ConsoleUtils.WaitForEscape();
+            ConsoleUtils.WaitForEscape();           
         }
 
         private static void DoGuess(int low, int high, int turns)
         {
-            char input = ' ';
             int guess = (low + high) / 2;
             Console.WriteLine("Is your number (h)igher than, (l)ower than or (e)qual to {0}?", guess);
 
             ConsoleKeyInfo cki = Console.ReadKey(true);
-            input = cki.KeyChar;
+            char input = cki.KeyChar;
 
             switch (input)
             {
                 case 'h':
-                    DoGuess(guess, high, turns++);
+                    DoGuess(guess, high, ++turns);
                     break;
                 case 'l':
-                    DoGuess(low, guess, turns++);
+                    DoGuess(low, guess, ++turns);
+                    break;
+                case 'e':
+                    Console.WriteLine("Guessed the correct number in {0} turns.", turns);
                     break;
             }
-        }
-
-        private static void DisplayEndText(int guesses)
-        {
-            //Console.WriteLine("Guessed the correct number in {0} turns.", NumberGuesser.NoGuesses);
-            Console.WriteLine("Guessed the correct number in {0} turns.", guesses);
         }
 
         private static void DisplayStartText()
         {
             Console.WriteLine("This program will try to guess a number between {0} and {1} that you pick.", Constants.RangeMin, Constants.RangeMax);
-        }
-
-        private static void DisplayPrompt()
-        {
-            Console.Write("> ");
         }
     }
 }

c1acb1791cc329228938ff9bf421c0c85e9040cb
Tim Collins <tim@timcollins.ie>
2014-10-14 09:17:24 +0100

    Add stopwatch project. Added infinite loop stoppable by Esc key.

---
+++ src/2-Diff-Stopwatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Util;
+
+namespace _2_Diff_Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            while (!done)
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    done = true;
+
+                Console.WriteLine("something...");
+            }
+
+
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

0a085476b9bf0ffdbf4731aeceae3b77d72b1817
Tim Collins <tim@timcollins.ie>
2014-10-15 08:21:21 +0100

    Added project files for Cipher challenge.

---
+++ src/3-Easy-Cipher/Program.cs
@@ -0,0 +1,18 @@
+﻿using Util;
+
+namespace _3_Easy_Cipher
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/
+    /// Write a program that can encrypt texts with an alphabetical caesar cipher. This 
+    /// cipher can ignore numbers, symbols, and whitespace.
+    /// For extra credit, add a "decrypt" function to your program!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

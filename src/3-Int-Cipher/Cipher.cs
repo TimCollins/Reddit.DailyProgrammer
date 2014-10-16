e95bbdef2bc0660fc3422a6f55ad1f36761ea8da
Tim Collins <tim@timcollins.ie>
2014-10-16 10:19:39 +0100

    Added initial implementation of http://www.reddit.com/r/dailyprogrammer/comments/pkwb1/2112012_challenge_3_intermediate/

---
+++ src/3-Int-Cipher/Cipher.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace _3_Int_Cipher
+{
+    class Cipher
+    {
+        public static string Encrypt(string input, string alphabet, string cypherbet)
+        {
+            StringBuilder output = new StringBuilder();
+
+            // For each letter in input, find the corresponding letter in cypherbet
+            // and append it to output.
+
+            foreach (char c in input)
+            {
+                //int index = cypherbet.IndexOf(c);
+                int index = alphabet.IndexOf(c);
+                output.Append(cypherbet[index]);
+            }
+
+            return output.ToString();
+        }
+
+        public static string Decrypt(string input, string cypherbet, string alphabet)
+        {
+            StringBuilder output = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                //int index = alphabet.IndexOf(c);
+                int index = cypherbet.IndexOf(c);
+                output.Append(alphabet[index]);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ src/3-Int-Cipher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Util;
+
+namespace _3_Int_Cipher
+{
+    /// <summary>
+    /// Create a program that can take a piece of text and encrypt it with an alphabetical 
+    /// substitution cipher. This can ignore white space, numbers, and symbols.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayHeader();
+
+            //Console.Write("Please enter some text to encrypt: ");
+            //string input = Console.ReadLine();
+
+            //Console.Write("Please enter a passphrase: ");
+            //string passPhrase = Console.ReadLine();
+
+            // For testing
+            string input = "FREDMCFRED";
+            string passPhrase = "ZXCV";
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            // Take chars from passphrase, remove repeated chars and prepend them to 
+            // the alphabet string. Remove the letters used in the pass phrase from the
+            // alphabet string.
+            string cypherbet = "ZXCVABDEFGHIJKLMNOPQRSTUWY";
+
+            Console.Write("Encrypted text: ");
+            string encrypted = Cipher.Encrypt(input, alphabet, cypherbet);
+            Console.WriteLine(encrypted);
+
+            Console.Write("Decrypted text: ");
+            Console.WriteLine(Cipher.Decrypt(encrypted, cypherbet, alphabet));
+
+            ConsoleUtils.WaitForEscape();
+        }
+
+        private static void DisplayHeader()
+        {
+            Console.WriteLine("This is an implementation of an alphabetic substitution cipher.");
+        }
+    }
+}

ba47194c2cc94851b8221f2250da635a31ca2c2b
Tim Collins <tim@timcollins.ie>
2014-10-16 11:44:22 +0100

    Added some test code.

--- src/3-Diff-Unscrambler/Program.cs
+++ src/3-Diff-Unscrambler/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Util;
 
 namespace _3_Diff_Unscrambler
@@ -18,31 +19,46 @@ namespace _3_Diff_Unscrambler
             
             // Something??
 
+            //List<string> words = new List<string>
+            //{
+            //    "mkeart",
+            //    "sleewa",
+            //    "edcudls",
+            //    "iragoge",
+            //    "usrlsle",
+            //    "nalraoci",
+            //    "nsdeuto",
+            //    "amrhat",
+            //    "inknsy",
+            //    "iferkna"
+            //};
+
+            //List<string> source = new List<string>();
+
+            //using (StreamReader sr = new StreamReader(fileName))
+            //{
+            //    while (!sr.EndOfStream)
+            //    {
+            //        source.Add(sr.ReadLine());
+            //    }
+            //}
+
+            //source = new List<string>(source.OrderBy(s => s));
+
+            // For testing
             List<string> words = new List<string>
             {
-                "mkeart",
-                "sleewa",
-                "edcudls",
-                "iragoge",
-                "usrlsle",
-                "nalraoci",
-                "nsdeuto",
-                "amrhat",
-                "inknsy",
-                "iferkna"
+                "derf",
+                "act",
+                "odg"
             };
 
-            List<string> source = new List<string>();
-
-            using (StreamReader sr = new StreamReader(fileName))
+            List<string> source = new List<string>
             {
-                while (!sr.EndOfStream)
-                {
-                    source.Add(sr.ReadLine());
-                }
-            }
-
-            source = new List<string>(source.OrderBy(s => s));
+                "fred",
+                "cat",
+                "dog"
+            };
 
             foreach (string s in words)
             {
@@ -52,8 +68,10 @@ namespace _3_Diff_Unscrambler
             ConsoleUtils.WaitForEscape();
         }
 
-        private static void Unscramble(string s, List<string> source)
+        public static void Unscramble(string s, List<string> source)
         {
+            // Make this private when the algorithm to use has been figued out.
+
             // Compare length of each word in source.
             // If matched
 
@@ -63,6 +81,18 @@ namespace _3_Diff_Unscrambler
                 {
                     // Do some funky rearranging of the letters to establish if
                     // s and source[i] are the same.
+                    var tmp = s.ToCharArray();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        char c = (char) (tmp[j] + 1);
+                        tmp[j + 1] = tmp[j];
+                        tmp[j] = c;
+
+                        if (tmp.ToString() == s)
+                        {
+                            break;
+                        }
+                    }
                 }
             }
         }

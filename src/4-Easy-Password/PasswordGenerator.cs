6574feb9c9af48c6826dc67cf673c580f516247f
Tim <tim@timcollins.ie>
2014-10-17 18:07:08 +0100

    Added initial version of project including placeholder methods.

---
+++ src/4-Easy-Password/PasswordGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _4_Easy_Password
+{
+    class PasswordGenerator
+    {
+        public static List<string> GeneratePasswords(int noPasswords, int passwordLength)
+        {
+            return new List<string>();
+        }
+    }
+}

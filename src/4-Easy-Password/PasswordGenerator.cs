c5595c73533e1b5d5c5a0db980691d5ab3e3e430
Tim <tim@timcollins.ie>
2014-10-17 18:37:21 +0100

    Added implementation.

---
+++ src/4-Easy-Password/PasswordGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_Easy_Password
+{
+    public class PasswordGenerator
+    {
+        private static readonly Random Random = new Random();
+        private const string SourceChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        public static List<string> GeneratePasswords(int noPasswords, int passwordLength)
+        {
+            List<string> passwords = new List<string>();
+
+            for (int i = 0; i < noPasswords; i++)
+            {
+                passwords.Add(GetPassword(passwordLength));
+            }
+
+            return passwords;
+        }
+
+        private static string GetPassword(int passwordLength)
+        {
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int r = Random.Next(0, SourceChars.Length - 1);
+                output.Append(SourceChars[r]);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ src/4-Easy-Password/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Util;
+
+namespace _4_Easy_Password
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pm6oj/2122012_challenge_4_easy/
+    /// Your challenge for today is to create a random password generator!
+    /// For extra credit, allow the user to specify the amount of passwords to generate.
+    /// For even more extra credit, allow the user to specify the length of the strings he 
+    /// wants to generate!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayStartText();
+            Console.Write("Please enter the number of passwords to generate: ");
+            int noPasswords = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the length of each password: ");
+            int passwordLength = Convert.ToInt32(Console.ReadLine());
+
+            List<string> passwords = PasswordGenerator.GeneratePasswords(noPasswords, passwordLength);
+
+            DisplayPasswords(passwords);
+
+            ConsoleUtils.WaitForEscape();
+        }
+
+        private static void DisplayPasswords(IReadOnlyList<string> passwords)
+        {
+            for (int i = 0; i < passwords.Count; i++)
+            {
+                Console.WriteLine("Password {0}: {1}", i + 1, passwords[i]);
+            }
+        }
+
+        private static void DisplayStartText()
+        {
+            Console.WriteLine("This is a password generator.");
+        }
+    }
+}

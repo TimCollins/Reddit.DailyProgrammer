8718fc380b353dcd9d9367fabf5c8e5eb05f1442
Tim <tim@timcollins.ie>
2014-10-18 12:32:23 +0100

    Added workable algorithm for the word scrambler challenge.

--- src/3-Diff-Unscrambler/Program.cs
+++ src/3-Diff-Unscrambler/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using Util;
 
 namespace _3_Diff_Unscrambler
 {
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pkwgf/2112012_challenge_3_difficult/
+    /// For this challenge, you need to write a program that will take the scrambled words from 
+    /// this post, and compare them against THIS WORD LIST to unscramble them. For bonus 
+    /// points, sort the words by length when you are finished.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -46,55 +50,66 @@ namespace _3_Diff_Unscrambler
             //source = new List<string>(source.OrderBy(s => s));
 
             // For testing
-            List<string> words = new List<string>
+            List<string> scrambledWords = new List<string>
             {
                 "derf",
                 "act",
                 "odg"
             };
 
-            List<string> source = new List<string>
+            List<string> unscrambledWords = new List<string>
             {
                 "fred",
                 "cat",
                 "dog"
             };
 
-            foreach (string s in words)
+            List<string> output = Unscramble(scrambledWords, unscrambledWords);
+
+            for (int i = 0; i < output.Count; i++)
             {
-                Unscramble(s, source);
+                Console.WriteLine("Scrambled {0} matches unscrambled {1}", scrambledWords[i], unscrambledWords[i]);
             }
 
             ConsoleUtils.WaitForEscape();
         }
 
-        public static void Unscramble(string s, List<string> source)
+        public static List<string> Unscramble(List<string> scrambledWords, List<string> unscrambledWords)
         {
             // Make this private when the algorithm to use has been figued out.
+            List<string> words = new List<string>();
 
             // Compare length of each word in source.
-            // If matched
+            // If matched then check that each letter in the source word can be found in a 
+            // sorted word. Output the sorted word to an array in the same position as the 
+            // unsorted word so that we can say unsorted[i] == sorted[i]
 
-            for (int i = 0; i < source.Count; i++)
+            foreach (string s in scrambledWords)
             {
-                if (s.Length == source[i].Length)
+                for (int i = 0; i < unscrambledWords.Count; i++)
                 {
-                    // Do some funky rearranging of the letters to establish if
-                    // s and source[i] are the same.
-                    var tmp = s.ToCharArray();
-                    for (int j = 0; j < s.Length; j++)
+                    if (s.Length == unscrambledWords[i].Length)
                     {
-                        char c = (char) (tmp[j] + 1);
-                        tmp[j + 1] = tmp[j];
-                        tmp[j] = c;
-
-                        if (tmp.ToString() == s)
+                        if (DoWordsMatch(s, unscrambledWords[i]))
                         {
-                            break;
+                            words.Add(unscrambledWords[i]);
                         }
                     }
                 }
             }
+
+            return words;
+        }
+
+        private static bool DoWordsMatch(string scrambledWord, string unscrambledWord)
+        {
+            foreach (char c in scrambledWord)
+            {
+                if (!unscrambledWord.Contains(c.ToString()))
+                    return false;
+            }
+
+            return true;
         }
     }
 }

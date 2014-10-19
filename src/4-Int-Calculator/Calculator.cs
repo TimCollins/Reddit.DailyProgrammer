4411cb65ac73289cd935040934c1825461df0192
Tim <tim@timcollins.ie>
2014-10-19 17:39:52 +0100

    Added unit tests to handle some basic expressions.

---
+++ src/4-Int-Calculator/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4_Int_Calculator
+{
+    public class Calculator
+    {
+        public static int Calculate(string input)
+        {
+            // Assume that input strings are going to consist of integers and operators only.
+            Regex.Replace(input, @"\s+", "");
+
+            // Handles things like 1 + 1
+            int? first = null;
+            char? op = null;
+            int? second = null;
+
+            foreach (char c in input)
+            {
+                if (Char.IsDigit(c))
+                {
+                    if (!first.HasValue)
+                    {
+                        first = Convert.ToInt32(c - 48);
+                    }
+                    else
+                    {
+                        second = Convert.ToInt32(c - 48);
+                    }
+                }
+                else if (IsOperator(c))
+                {
+                    op = c;
+                }
+            }
+
+            // If the input string hasn't been parsed then throw an exception.
+            if (!first.HasValue || !op.HasValue || !second.HasValue)
+            {
+                throw new ArgumentException("input");   
+            }
+
+            return Evaluate(first.Value, op.Value, second.Value);
+        }
+
+        private static bool IsOperator(char c)
+        {
+            return "+-/*".Contains(c.ToString());
+        }
+
+        private static int Evaluate(int first, char op, int second)
+        {
+            switch (op)
+            {
+                case '+':
+                    return first + second;
+                case '-':
+                    return first - second;
+                case '*':
+                    return first * second;
+                case '/':
+                    return first / second;
+            }
+            throw new ArgumentException("op");
+        }
+    }
+}
---
+++ src/UnitTests/IntCalculatorTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using _4_Int_Calculator;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class IntCalculatorTests
+    {
+        [Test]
+        public void VerifyBasicAddition()
+        {
+            const string input = "5 + 5";
+
+            int output = Calculator.Calculate(input);
+
+            Assert.AreEqual(10, output);
+        }
+
+        [Test]
+        public void VerifyBasicDivision()
+        {
+            const string input = "5 / 5";
+
+            int output = Calculator.Calculate(input);
+
+            Assert.AreEqual(1, output);
+        }
+
+        [Test]
+        public void VerifyBasicSubtraction()
+        {
+            const string input = "5 - 5";
+
+            int output = Calculator.Calculate(input);
+
+            Assert.AreEqual(0, output);
+        }
+
+        [Test]
+        public void VerifyBasicMultiplication()
+        {
+            const string input = "5 * 5";
+
+            int output = Calculator.Calculate(input);
+
+            Assert.AreEqual(25, output);
+        }
+
+        //[Test]
+        //public void VerifyExpectedOutput()
+        //{
+        //    const string input = "5 * 5 + 4";
+
+        //    int output = Calculator.Calculate(input);
+
+        //    Assert.AreEqual(29, output);
+        //}
+    }
+}

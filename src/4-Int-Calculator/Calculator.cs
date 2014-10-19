63ec34a466eef01ba3dc25daff4db460c6cfa7cf
Tim <tim@timcollins.ie>
2014-10-19 19:47:03 +0100

    Code tidy-up.

--- src/4-Int-Calculator/Calculator.cs
+++ src/4-Int-Calculator/Calculator.cs
@@ -11,7 +11,6 @@ namespace _4_Int_Calculator
             // Assume that input strings are going to consist of integers and operators only.
             // e.g. (5*5+4)
             // First and last chars will always be numbers.
-            // Put the numbers and operators into a list.
             Regex.Replace(input, @"\s+", "");
 
             List<int> numbers = new List<int>();
@@ -19,38 +18,6 @@ namespace _4_Int_Calculator
             ParseInput(input, numbers, operators);
 
             return Evaluate(numbers, operators);
-
-            // Handles things like 1 + 1
-            //int? first = null;
-            //char? op = null;
-            //int? second = null;
-
-            //foreach (char c in input)
-            //{
-            //    if (Char.IsDigit(c))
-            //    {
-            //        if (!first.HasValue)
-            //        {
-            //            first = Convert.ToInt32(c - 48);
-            //        }
-            //        else
-            //        {
-            //            second = Convert.ToInt32(c - 48);
-            //        }
-            //    }
-            //    else if (IsOperator(c))
-            //    {
-            //        op = c;
-            //    }
-            //}
-
-            //// If the input string hasn't been parsed then throw an exception.
-            //if (!first.HasValue || !op.HasValue || !second.HasValue)
-            //{
-            //    throw new ArgumentException("input");   
-            //}
-
-            //return Evaluate(first.Value, op.Value, second.Value);
         }
 
         private static int Evaluate(List<int> numbers, List<char> operators)
@@ -59,7 +26,6 @@ namespace _4_Int_Calculator
             // If the first operator is + or - and the second operator is / or * then the third
             // number should be used in the * or / expression with the second number 
             // and then the first operator and number with the result.
-
 
             char firstOp = operators[0];
             char? secondOp = operators.Count > 1 ? operators[1] : (char?) null;
@@ -87,8 +53,6 @@ namespace _4_Int_Calculator
                     ? part1 + part2 
                     : part2;
             }
-
-            return part1 + part2;
         }
 
         private static int GetResult(int first, int second, char op)

d6ac97a951e288dcb1a7f26034632d2fa01341ff
Tim <tim@timcollins.ie>
2014-10-19 17:03:19 +0100

    Added initial version of 4-Int-Calculator Removed unused references.

---
+++ src/4-Int-Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using Util;
+
+namespace _4_Int_Calculator
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pm6sq/2122012_challenge_4_intermediate/
+    /// Create a calculator program that will take an input, following normal calculator 
+    /// input (5 * 5 + 4) and give an answer (29). This calculator should use all four operators.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

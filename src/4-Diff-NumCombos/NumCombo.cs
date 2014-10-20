704a5a076615bad0dfd0a4a12d0583ebfd2abf00
Tim <tim@timcollins.ie>
2014-10-20 11:42:16 +0100

    Added initial version of input parser and associated unit tests.

--- src/4-Diff-NumCombos/NumCombo.cs
+++ src/4-Diff-NumCombos/NumCombo.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _4_Diff_NumCombos
 {
-    class NumCombo
+    public class NumCombo
     {
         public static List<int> ParseInput(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException(input);
+            }
+
             // Take a string like 2,3,4
             // and return a list of just the integers.
-            return null;
+            input = Regex.Replace(input, @"\s+", "");
+
+            string[] numbers = input.Split(',');
+
+            List<int> output = new List<int>();
+
+            foreach (string s in numbers)
+            {
+                output.Add(Convert.ToInt32(s));
+            }
+            
+            return output;
         }
 
         public static List<string> FindRelations(List<int> numbers)
--- src/UnitTests/NumComboTests.cs
+++ src/UnitTests/NumComboTests.cs
@@ -1,9 +1,44 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using _4_Diff_NumCombos;
 
 namespace UnitTests
 {
     [TestFixture]
     class NumComboTests
     {
+        [Test]
+        public void WhenInputIsBlankExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => NumCombo.ParseInput(null));
+        }
+        [Test]
+        public void VerifyParseInputWithSpaces()
+        {
+            const string input = "2, 3, 4";
+
+            List<int> output = NumCombo.ParseInput(input);
+
+            Assert.AreEqual(3, output.Count);
+            Assert.AreEqual(2, output[0]);
+            Assert.AreEqual(3, output[1]);
+            Assert.AreEqual(4, output[2]);
+        }
+
+        [Test]
+        public void VerifyParseInputNoSpaces()
+        {
+            // Given a string like "2,3,4"
+            // return a list of just the integers.
+            const string input = "2,3,4";
+
+            List<int> output = NumCombo.ParseInput(input);
+
+            Assert.AreEqual(3, output.Count);
+            Assert.AreEqual(2, output[0]);
+            Assert.AreEqual(3, output[1]);
+            Assert.AreEqual(4, output[2]);
+        }
     }
 }

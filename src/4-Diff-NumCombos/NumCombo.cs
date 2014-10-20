09b51910ae9f2b3c2e57fe15611ed439005fc19e
Tim <tim@timcollins.ie>
2014-10-20 11:19:30 +0100

    Added initial version of project.

---
+++ src/4-Diff-NumCombos/NumCombo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace _4_Diff_NumCombos
+{
+    class NumCombo
+    {
+        public static List<int> ParseInput(string input)
+        {
+            // Take a string like 2,3,4
+            // and return a list of just the integers.
+            return null;
+        }
+
+        public static List<string> FindRelations(List<int> numbers)
+        {
+            // Given a list of numbers
+            // return a set of strings showing how the numbers are related
+            // e.g. (5, 3, 15) should return "5 * 3 = 15", "15 / 3 = 5", "15 / 5 = 15"
+            return null;
+        }
+    }
+}
---
+++ src/4-Diff-NumCombos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Util;
+
+namespace _4_Diff_NumCombos
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pm7g7/2122012_challange_4_difficult/
+    /// Create a program that will take a series of numbers (5, 3, 15), and find how those numbers 
+    /// can add, subtract, multiply, or divide in various ways to relate to each other.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayHeader();
+
+            Console.Write("Please enter some comma-separated numbers: ");
+            string input = Console.ReadLine();
+            List<int> numbers = NumCombo.ParseInput(input);
+            List<string> output = NumCombo.FindRelations(numbers);
+
+            ShowOutput(numbers, output);
+            ConsoleUtils.WaitForEscape();
+        }
+
+        private static void ShowOutput(List<int> numbers, List<string> output)
+        {
+            Console.WriteLine("The input was ");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write("{0}", numbers[i]);
+                Console.Write(i < numbers.Count - 1 ? ", " : "");
+            }
+
+            Console.WriteLine("The output is ");
+            foreach (string s in output)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void DisplayHeader()
+        {
+            Console.WriteLine("This program will take a series of numbers and show how " +
+                "those numbers relate to each other.");
+        }
+    }
+}

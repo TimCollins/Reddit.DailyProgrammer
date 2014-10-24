8f1374f2279f39b73a63707274e473cef93ee05d
Tim <tim@timcollins.ie>
2014-10-24 21:35:10 +0100

    Renamed unit test file. Added basic passing test.

--- src/6-Int-Remove-Dup/DuplicateRemover.cs
+++ src/6-Int-Remove-Dup/DuplicateRemover.cs
@@ -1,10 +1,40 @@
-﻿namespace _6_Int_Remove_Dup
+﻿using System.Text;
+
+namespace _6_Int_Remove_Dup
 {
     public class DuplicateRemover
     {
         public static string Remove(string input)
         {
-            return string.Empty;
+            StringBuilder output = new StringBuilder();
+
+            // Split the string in half and compare one half to the other.
+            string first = input.Substring(0, input.Length / 2);
+            string second = input.Substring(input.Length / 2);
+
+            if (DoWordsMatch(first, second))
+            {
+                output.Append(first);
+            }
+
+            return output.ToString();
+        }
+
+        private static bool DoWordsMatch(string first, string second)
+        {
+            foreach (char c in first)
+            {
+                if (!second.Contains(c.ToString()))
+                    return false;
+            }
+
+            foreach (char c in second)
+            {
+                if (!first.Contains(c.ToString()))
+                    return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ src/UnitTests/DuplicatesRemovalTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using _6_Int_Remove_Dup;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class DuplicatesRemovalTests
+    {
+        [Test]
+        public void VerifySimpleOutput()
+        {
+            string output = DuplicateRemover.Remove("hellohello");
+
+            Assert.AreEqual("hello", output);
+        }
+
+        [Test]
+        public void VerifyOutput()
+        {
+            string output = DuplicateRemover.Remove("aaatestBlaBlatestBlaBla");
+
+            // The second occurrence of "test" and the third and fourth occurrences of "Bla" 
+            // should be removed.
+
+            Assert.AreEqual("aaatestBlaBla", output);
+        }
+    }
+}

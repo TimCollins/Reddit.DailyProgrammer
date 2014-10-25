8e0a68c01dbacd4d57283a3f5effe909290a6816
Tim <tim@timcollins.ie>
2014-10-25 12:45:57 +0100

    Continued implementation.

--- src/6-Diff-Nim-Player/NimPlayer.cs
+++ src/6-Diff-Nim-Player/NimPlayer.cs
@@ -1,14 +1,94 @@
-﻿namespace _6_Diff_Nim_Player
+﻿using System;
+
+namespace _6_Diff_Nim_Player
 {
     class NimPlayer
     {
         public static int HeapA { get; set; }
         public static int HeapB { get; set; }
         public static int HeapC { get; set; }
+        
+        private static readonly Random _random = new Random();
 
         public static bool HeapsEmpty()
         {
             return HeapA == 0 && HeapB == 0 && HeapC == 0;
+        }
+
+        public static void DoMove(string heap, int quantity)
+        {
+            if (heap == "A")
+            {
+                HeapA -= quantity;
+                return;
+            }
+
+            if (heap == "B")
+            {
+                HeapB -= quantity;
+                return;
+            }
+
+            if (heap == "C")
+            {
+                HeapC -= quantity;
+                return;
+            }
+        }
+
+        public static string PickHeap()
+        {
+            bool validChoice = false;
+            int r = -1;
+
+            while (!validChoice)
+            {
+                r = _random.Next(0, 3);
+                validChoice = ValidateHeap(r);
+            }
+
+            if (r == 0)
+            {
+                return "A";
+            }
+
+            return r == 1 ? "B" : "C";
+
+        }
+
+        private static bool ValidateHeap(int heap)
+        {
+            if (heap == 0 && HeapA == 0)
+            {
+                return false;
+            }
+
+            if (heap == 1 && HeapB == 0)
+            {
+                return false;
+            }
+
+            if (heap == 2 && HeapC == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int PickQuantity(string heap)
+        {
+            if (heap == "A")
+            {
+                return _random.Next(1, HeapA + 1);
+            }
+
+            if (heap == "B")
+            {
+                return _random.Next(1, HeapB + 1);
+            }
+
+            return _random.Next(1, HeapC + 1);
         }
     }
 }
--- src/6-Diff-Nim-Player/Program.cs
+++ src/6-Diff-Nim-Player/Program.cs
@@ -16,16 +16,55 @@ namespace _6_Diff_Nim_Player
             Init();
             
             bool done = false;
+            bool aiMove = false;
 
             while (!done)
             {
+                ShowHeapDetails();
+                if (aiMove)
+                {
+                    // Computer move.
+                    // Pick a random heap
+                    string aiHeap = NimPlayer.PickHeap();                    
+                    // Pick a random quantity
+                    int aiQuantity = NimPlayer.PickQuantity(aiHeap);
+                    NimPlayer.DoMove(aiHeap, aiQuantity);
+
+                    Console.WriteLine("AI takes {0} from Heap {1}", aiQuantity, aiHeap);
+                    aiMove = false;
+                }
+                else
+                {
+                    // Human move
+                    ShowPrompt();
+                    string input = Console.ReadLine();
+
+                    string heap = input.Substring(0, 1);
+                    int quantity = Convert.ToInt32(input.Substring(1, 1));
+
+                    NimPlayer.DoMove(heap.ToUpper(), quantity);
+
+                    aiMove = true;
+                }
+
                 if (NimPlayer.HeapsEmpty())
                 {
                     done = true;
                 }
             }
 
+            Console.WriteLine("Done.");
             ConsoleUtils.WaitForEscape();
+        }
+
+        private static void ShowHeapDetails()
+        {
+            Console.WriteLine("A: {0}\tB: {1}\tC: {2}", NimPlayer.HeapA, NimPlayer.HeapB, NimPlayer.HeapC);
+        }
+
+        private static void ShowPrompt()
+        {
+            Console.Write("Enter heap and quantity e.g. \"A1\":");
         }
 
         private static void Init()

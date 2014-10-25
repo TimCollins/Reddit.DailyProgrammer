cbdb0720d63b1312343970a72f3df5eb270d1be4
Tim <tim@timcollins.ie>
2014-10-25 12:24:28 +0100

    Added initial project structure.

---
+++ src/6-Diff-Nim-Player/NimPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace _6_Diff_Nim_Player
+{
+    class NimPlayer
+    {
+        public static int HeapA { get; set; }
+        public static int HeapB { get; set; }
+        public static int HeapC { get; set; }
+
+        public static bool HeapsEmpty()
+        {
+            return HeapA == 0 && HeapB == 0 && HeapC == 0;
+        }
+    }
+}
---
+++ src/6-Diff-Nim-Player/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Util;
+
+namespace _6_Diff_Nim_Player
+{
+    /// <summary>
+    /// See http://www.reddit.com/r/dailyprogrammer/comments/pp7vo/2142012_challenge_6_difficult/
+    /// Create an AI that will play NIM (http://en.wikipedia.org/wiki/Nim).
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayStartText();
+
+            Init();
+            
+            bool done = false;
+
+            while (!done)
+            {
+                if (NimPlayer.HeapsEmpty())
+                {
+                    done = true;
+                }
+            }
+
+            ConsoleUtils.WaitForEscape();
+        }
+
+        private static void Init()
+        {
+            NimPlayer.HeapA = 3;
+            NimPlayer.HeapB = 4;
+            NimPlayer.HeapC = 5;
+        }
+
+
+        private static void DisplayStartText()
+        {
+            Console.WriteLine("This program will play Nim.");
+        }
+    }
+}

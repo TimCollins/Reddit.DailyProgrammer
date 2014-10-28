0c01e8038fc5c49d90e2ba7e6af3e007c306a66f
Tim <tim@timcollins.ie>
2014-10-28 20:01:55 +0000

    Fleshing out the Convert method.

--- src/8-Int-Numbers-To-English/NumberConverter.cs
+++ src/8-Int-Numbers-To-English/NumberConverter.cs
@@ -4,6 +4,33 @@
     {
         public static string Convert(int number)
         {
+            // Need to break the number down into unit, tens, hundreds.
+            // 100 / 10 = 10
+            // 100 % 10 = 0
+            switch (number)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+            }
+
             return string.Empty;
         }
     }

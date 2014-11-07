0748599f990a1fe0685acf6bd20a6c7794f5d3ae
Tim <tim@timcollins.ie>
2014-11-07 17:26:13 +0000

    Initial check in.

---
+++ src/9-Int-String-Replace/Program.cs
@@ -0,0 +1,12 @@
+﻿using Util;
+
+namespace _9_Int_String_Replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/9-Int-String-Replace/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("9-Int-String-Replace")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9-Int-String-Replace")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60f3cfbf-8370-43e5-bd60-0817f16a2e51")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

11198834ce00e3b477190b19b2d2efc885a7d5e9
Tim <tim@timcollins.ie>
2014-11-07 20:28:49 +0000

    Added replacer functionality and source data.

--- src/9-Int-String-Replace/Program.cs
+++ src/9-Int-String-Replace/Program.cs
@@ -1,4 +1,6 @@
-﻿using Util;
+﻿using System;
+using System.IO;
+using Util;
 
 namespace _9_Int_String_Replace
 {
@@ -6,6 +8,20 @@ namespace _9_Int_String_Replace
     {
         static void Main(string[] args)
         {
+            string fileName = Directory.GetCurrentDirectory() + "\\mlkdream.txt";
+
+            Console.WriteLine("Enter string to search for.");
+            Console.Write("> ");
+            string searchString = Console.ReadLine();
+
+            Console.WriteLine("Enter string to replace.");
+            Console.Write("> ");
+            string replaceString = Console.ReadLine();
+
+            int replaceCount = Replacer.Replace(fileName, searchString, replaceString);
+
+            Console.WriteLine("Replaced {0} occurrences of {1} in {2} with {3}.", replaceCount, searchString, fileName, replaceString);
+
             ConsoleUtils.WaitForEscape();
         }
     }
---
+++ src/9-Int-String-Replace/Replacer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace _9_Int_String_Replace
+{
+    public static class Replacer
+    {
+        public static int Replace(string fileName, string searchString, string replaceString)
+        {
+            List<string> lines = ReadLines(fileName);
+            int replaceCount = 0;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i].Contains(searchString))
+                {
+                    lines[i] = lines[i].Replace(searchString, replaceString);
+                    replaceCount++;
+                }
+            }
+
+            SaveLines(lines);
+
+            return replaceCount;
+        }
+
+        private static void SaveLines(IEnumerable<string> lines)
+        {
+            string fileName = Directory.GetCurrentDirectory() + "\\output.txt";
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach (string line in lines)
+                {
+                    sw.Write(line);    
+                }
+            }
+        }
+
+        private static List<string> ReadLines(string fileName)
+        {
+            List<string> lines =new List<string>();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+    }
+}

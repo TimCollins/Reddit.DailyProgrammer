096036e4c5daa228f56d22635cce80b25140d2e0
Tim <tim@timcollins.ie>
2017-08-06 16:05:14 +0100

    Initial check in of project and unit test class.

---
+++ src/10-Easy-Validation/Program.cs
@@ -0,0 +1,12 @@
+﻿using Util;
+
+namespace _10_Easy_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/UnitTests/PhoneValidation.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class PhoneValidation
+    {
+        [Test]
+        public void AllDigitsShouldBeValid()
+        {
+            const string input = "1234567890";
+            var isValid = PhoneNumberValidator.Validate(input);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}

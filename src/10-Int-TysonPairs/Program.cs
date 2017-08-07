b44d3b768d60fce863fc76aa3a7a2006c01c766c
Tim <tim@timcollins.ie>
2017-08-07 14:45:30 +0100

    Fixed a bug in the NumberConverter project. Added NumberConverter as a reference.

--- src/10-Int-TysonPairs/Program.cs
+++ src/10-Int-TysonPairs/Program.cs
@@ -1,11 +1,20 @@
-﻿using Util;
+﻿using System;
+using Util;
+using _8_Int_Numbers_To_English;
 
 namespace _10_Int_TysonPairs
 {
     class Program
     {
+        // Not only does 12+1=11+2, but the letters “twelve plus one” rearrange to give you 
+        // “eleven plus two”
+
+        // Your task is to write a program that finds equations similar to Tyson’s that form 
+        // anagrams both in their symbols and in their letters.
         static void Main(string[] args)
         {
+            var output = NumberConverter.Convert(50);
+            Console.WriteLine(output);
             ConsoleUtils.WaitForEscape();
         }
     }

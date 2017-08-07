8f7a3a24f1726748760ebd947d2dd7730f09972a
Tim <tim@timcollins.ie>
2017-08-07 15:59:51 +0100

    Trying and failing to come up with an algorithm.

--- src/10-Int-TysonPairs/Program.cs
+++ src/10-Int-TysonPairs/Program.cs
@@ -13,9 +13,27 @@ namespace _10_Int_TysonPairs
         // anagrams both in their symbols and in their letters.
         static void Main(string[] args)
         {
+            var checker = new TysonPairChecker();
+
+            for (var i = 1; i < 51; i++)
+            {
+                for (var j = 1; j < 51; j++)
+                {
+                    if (checker.IsPair(i, j))
+                    {
+                        Console.WriteLine(FormatOutput(i, j));
+                    }
+                }
+            }
             var output = NumberConverter.Convert(50);
             Console.WriteLine(output);
             ConsoleUtils.WaitForEscape();
+        }
+
+        private static string FormatOutput(int first, int second)
+        {
+            NumberConverter.Convert(50);
+            return string.Empty;
         }
     }
 }

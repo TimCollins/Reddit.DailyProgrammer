2b3175c0074870f5071acb89389c9c2cfaed064b
Tim <tim@timcollins.ie>
2017-10-21 18:21:45 +0100

    Added project skeleton.

---
+++ src/9-Hard-Pattern/PatternGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace _9_Hard_Pattern
+{
+    internal static class PatternGenerator
+    {
+        public static string Calculate(int max)
+        {
+            return "1 1 1 2";
+        }
+    }
+}
---
+++ src/9-Hard-Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Util;
+
+namespace _9_Hard_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int max = 40;
+            string output = PatternGenerator.Calculate(max);
+
+            Console.WriteLine("Iteration {0} is {1}", max, output);
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

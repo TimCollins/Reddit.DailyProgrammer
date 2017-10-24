0d21211d1c10c247107022936de85716ddd68dbd
Tim <tim@timcollins.ie>
2017-10-24 21:23:22 +0100

    Refactored tests.

--- src/UnitTests/11-DayOfWeek.cs
+++ src/UnitTests/11-DayOfWeek.cs
@@ -6,35 +6,41 @@ namespace UnitTests
     [TestFixture]
     public class DayOfWeekTests
     {
-        // Tests: args < 3, args > 3
+        private DayOfWeekCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new DayOfWeekCalculator();
+        }
         // Test the first arg to be from 0 - 6 inclusive
         // The the second to be from 0 - 11 inclusive
         // The third to be from 1970 - 2038 inclusive
         [Test]
         public void LessThanThreeArgsWillFail()
         {
-            var calculator = new DayOfWeekCalculator();
-            var args = new[] {"first", "second"};
+            var args = new[] { "first", "second" };
 
-            Assert.IsFalse(calculator.IsValidCommandLine(args));
+            Assert.IsFalse(_calculator.IsValidCommandLine(args));
         }
 
         [Test]
         public void MoreThanThreeArgsWillFail()
         {
-            var calculator = new DayOfWeekCalculator();
             var args = new[] { "first", "second", "third", "fourth" };
 
-            Assert.IsFalse(calculator.IsValidCommandLine(args));
+            Assert.IsFalse(_calculator.IsValidCommandLine(args));
         }
 
         [Test]
         public void ExactlyThreeArgsWillPass()
         {
-            var calculator = new DayOfWeekCalculator();
             var args = new[] { "first", "second", "third" };
 
-            Assert.IsTrue(calculator.IsValidCommandLine(args));
+            Assert.IsTrue(_calculator.IsValidCommandLine(args));
         }
+
+
+
     }
 }

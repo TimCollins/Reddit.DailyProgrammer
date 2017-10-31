091966ae9a313c89bca7247b08a826e40410a02c
Tim <tim@timcollins.ie>
2017-10-31 14:51:37 +0000

    Added some initial tests.

--- src/11-Int-Upside-Up/UpsideUpCalculator.cs
+++ src/11-Int-Upside-Up/UpsideUpCalculator.cs
@@ -1,4 +1,7 @@
-﻿namespace _11_Int_Upside_Up
+﻿using System;
+using System.Linq;
+
+namespace _11_Int_Upside_Up
 {
     public class UpsideUpCalculator
     {
@@ -10,6 +13,41 @@
         public int GetTotal(int max)
         {
             return 1;
+        }
+
+        public bool IsUpsideUp(int input)
+        {
+            // 1961, 689, 0690 should succeed
+            // 2017 should fail
+            // These digits can be flipped:
+            // 1 => 1
+            // 6 => 9
+            // 8 => 8
+            // 9 => 6
+            // 0 => 0
+
+            var digits = input.ToString().ToCharArray();
+            var reversed = digits.Reverse().ToArray();
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                var digit = digits[i];
+
+                if (!CanFlip(digit))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CanFlip(char digit)
+        {
+            var number = (int) char.GetNumericValue(digit);
+            var canFlip = new[] { 1, 6, 8, 9, 0 };
+
+            return canFlip.Contains(number);
         }
     }
 }
--- src/UnitTests/UpsideUpNumbersTests.cs
+++ src/UnitTests/UpsideUpNumbersTests.cs
@@ -1,16 +1,41 @@
 ﻿using NUnit.Framework;
+using _11_Int_Upside_Up;
 
 namespace UnitTests
 {
     [TestFixture]
     public class UpsideUpNumbersTests
     {
+        private UpsideUpCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new UpsideUpCalculator();
+        }
+
+        [Test]
+        public void SupportedSingleDigitsShouldSucceed()
+        {
+            const int input = 1;
+
+            Assert.IsTrue(_calculator.IsUpsideUp(input));
+        }
+
+        [Test]
+        public void UnsupportedSingleDigitsShouldFail()
+        {
+            const int input = 7;
+
+            Assert.IsFalse(_calculator.IsUpsideUp(input));
+        }
+
         [Test]
         public void ShouldSucceed1961()
         {
-            var input = 1961;
+            const int input = 1961;
 
-
+            Assert.IsTrue(_calculator.IsUpsideUp(input));
         }
     }
 }

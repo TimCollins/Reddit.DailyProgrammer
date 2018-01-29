5b88a94623a3ba678ac8d83a8b225d5b78f1df08
Tim <tim@timcollins.ie>
2018-01-29 20:39:42 +0000

    Added tests.

--- src/UnitTests/12-Factors.cs
+++ src/UnitTests/12-Factors.cs
@@ -1,14 +1,46 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using _12_Int_Factors;
 
 namespace UnitTests
 {
     [TestFixture]
     public class Factors
     {
-        [Test]
-        public void Something()
+        private FactorCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.AreEqual(1, 1);
+            _calculator = new FactorCalculator();
+        }
+
+        [Test]
+        public void FactorOfOneIsOne()
+        {
+            var factors = _calculator.GetFactors(1);
+
+            Assert.AreEqual(1, factors.Count);
+            Assert.AreEqual(1, factors[0]);
+        }
+
+        [Test]
+        public void FactorsOfTwelve()
+        {
+            var factors = _calculator.GetFactors(12);
+            factors.Sort();
+
+            var expected = new List<int> {1, 2, 3, 4, 6, 12};
+            // An intersection is a set of elements that are common to both
+            var duplicates = factors.Intersect(expected).ToList();
+            // Get all factors except those that are in the expected list
+            // Obviously this list should have no items
+            var distinct = factors.Except(expected).ToList();
+
+            Assert.AreEqual(6, factors.Count);
+            Assert.AreEqual(6, duplicates.Count);
+            Assert.AreEqual(0, distinct.Count);            
         }
     }
 }

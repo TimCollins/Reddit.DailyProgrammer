e4cb3abe3a33d2f4a40d7e13a1e19fc21ca1e64c
Tim <tim@timcollins.ie>
2018-01-29 20:29:04 +0000

    Added calculator.

--- src/12-Int-Factors/FactorCalculator.cs
+++ src/12-Int-Factors/FactorCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace _12_Int_Factors
 {
@@ -6,7 +7,27 @@ namespace _12_Int_Factors
     {
         public List<int> GetFactors(int number)
         {
-            return new List<int>();
+            var factors = new List<int>();
+            // Loop to the square root (rounded) of the number
+            var max = (int)Math.Sqrt(number);
+
+            for (var factor = 1; factor <= max; ++factor)
+            {
+                // If the remainder is 0 then add it to the list
+                // and also add
+                if (number % factor == 0)
+                {
+                    factors.Add(factor);
+                    if (factor != number / factor)
+                    {
+                        factors.Add(number / factor);
+                    }
+                }
+            }
+
+            factors.Sort();
+
+            return factors;
         }
     }
 }

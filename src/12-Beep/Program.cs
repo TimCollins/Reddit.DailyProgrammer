7391e52c4001c2f95055f9df488a5d659b85d81b
Tim <tim@timcollins.ie>
2018-02-05 18:56:42 +0000

    Initial version of beeping program. No duration or octaves are used.

---
+++ src/12-Beep/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Util;
+
+namespace _12_Beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("You must enter some notes!");
+            }
+            
+            new NotePlayer().Play(args);
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

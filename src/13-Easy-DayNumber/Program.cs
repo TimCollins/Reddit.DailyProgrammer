9ea8c03f4e06cce8d21656fd18b90e7ec3187a57
Tim <tim@timcollins.ie>
2018-02-08 20:52:15 +0000

    Initial commit of project files.

---
+++ src/13-Easy-DayNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using Util;
+
+namespace _13_Easy_DayNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/UnitTests/13-DayNumberTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class _13_DayNumberTests
+    {
+        [Test]
+        public void Something()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

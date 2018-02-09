f198908466f9b525ffaa7fb44d03b5ac53a9f1cf
Tim <tim@timcollins.ie>
2018-02-09 18:27:05 +0000

    Added unit tests.

--- src/UnitTests/13-DayNumberTests.cs
+++ src/UnitTests/13-DayNumberTests.cs
@@ -1,14 +1,45 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using _13_Easy_DayNumber;
 
 namespace UnitTests
 {
     [TestFixture]
     public class _13_DayNumberTests
     {
-        [Test]
-        public void Something()
+        private DayNumberCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.AreEqual(1, 1);
+            _calculator = new DayNumberCalculator();    
+        }
+
+        [Test]
+        public void January1ShouldReturn1()
+        {
+            var input = new DateTime(2018, 1, 1);
+            var output = _calculator.GetDayNumber(input);
+
+            Assert.AreEqual(1, output);
+        }
+
+        [Test]
+        public void December31ShouldReturn365()
+        {
+            var input = new DateTime(2018, 12, 31);
+            var output = _calculator.GetDayNumber(input);
+
+            Assert.AreEqual(365, output);
+        }
+
+        [Test]
+        public void DayNumberShouldWorkForPreviousYear()
+        {
+            var input = new DateTime(2017, 12, 31);
+            var output = _calculator.GetDayNumber(input);
+
+            Assert.AreEqual(365, output);
         }
     }
 }

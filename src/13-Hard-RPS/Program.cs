a043966a15ed37d183d08538424369c2598b7540
Tim <tim@timcollins.ie>
2018-02-09 19:00:39 +0000

    Initial commit

---
+++ src/13-Hard-RPS/Program.cs
@@ -0,0 +1,16 @@
+﻿using Util;
+
+namespace _13_Hard_RPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a rock - paper - scissors program, however, there should be no user 
+            // input. The computer should play against itself. Make the program keep score, 
+            // and for extra credit, give the option to "weigh" the chances, so one AI will 
+            // win more often.
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}

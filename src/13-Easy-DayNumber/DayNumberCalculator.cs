17bc85aee7db3a1d27c1c416e9ddab8c6ce93e24
Tim <tim@timcollins.ie>
2018-02-09 18:27:19 +0000

    Added implementation and some basic error handling.

---
+++ src/13-Easy-DayNumber/DayNumberCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _13_Easy_DayNumber
+{
+    public class DayNumberCalculator
+    {
+        private DateTime _date;
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public int GetDayNumber(string input)
+        {
+            if (!Parse(input))
+            {
+                return -1;
+            }
+
+            try
+            {
+                _date = new DateTime(_year, _month, _day);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return GetDayNumber(_date);
+        }
+
+        private bool Parse(string input)
+        {
+            var parts = input.Split('/');
+            if (parts.Length != 3)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(parts[0], out _year))
+            {
+                return false;
+            }
+
+            if (!int.TryParse(parts[1], out _month))
+            {
+                return false;
+            }
+
+            if (!int.TryParse(parts[2], out _day))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public int GetDayNumber(DateTime date)
+        {
+            var yearEnd = new DateTime(date.Year, 12, 31);
+            var ts = yearEnd - date;
+
+            return 365 - ts.Days;
+        }
+
+        public bool IsValidCommandLine(string[] args)
+        {
+            return args.Length == 1;
+        }
+    }
+}
--- src/13-Easy-DayNumber/Program.cs
+++ src/13-Easy-DayNumber/Program.cs
@@ -1,4 +1,5 @@
-﻿using Util;
+﻿using System;
+using Util;
 
 namespace _13_Easy_DayNumber
 {
@@ -6,6 +7,21 @@ namespace _13_Easy_DayNumber
     {
         static void Main(string[] args)
         {
+            // Take in a date on the command-line
+            // e.g. 8/2/2018 represents February 8th, 2018
+            // Then calculate the day number.
+            // January 1st should return 1
+            // December 31st should return 365 for a non leap year
+            var calculator = new DayNumberCalculator();
+
+            if (!calculator.IsValidCommandLine(args))
+            {
+                Console.WriteLine("Invalid command line.\nPlease specify a valid date in yyyy/mm/dd format");
+                return;
+            }
+
+            var output = calculator.GetDayNumber(args[0]);
+            Console.WriteLine("Day number for {0} is {1}", args[0], output);
             ConsoleUtils.WaitForEscape();
         }
     }

08cf447362ec3c74f9e2fc5caed4eee577b4379e
Tim <tim@timcollins.ie>
2018-02-09 18:54:12 +0000

    Added implementation

---
+++ src/13-Int-Reverser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Util;
+
+namespace _13_Int_Reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take a string from the command-line
+            // Reverse it
+            // Write it out to reverse.txt
+            var reverser = new Reverser();
+            if (!reverser.IsValidCommandLine(args))
+            {
+                Console.WriteLine("Invalid command line.\nPlease specify a line of text to reverse.");
+                return;
+            }
+
+            var output = reverser.Reverse(args[0]);
+            reverser.Write(output);
+
+            Console.WriteLine("Reversed string written!");
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/13-Int-Reverser/Reverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _13_Int_Reverser
+{
+    public class Reverser
+    {
+        public bool IsValidCommandLine(string[] args)
+        {
+            return args.Length == 1;
+        }
+
+        public string Reverse(string input)
+        {
+            var chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        public void Write(string s)
+        {
+            var filename = Environment.CurrentDirectory + "\\output.txt";
+
+            using (var sw = new StreamWriter(filename))
+            {
+                sw.Write(s);
+            }
+        }
+    }
+}

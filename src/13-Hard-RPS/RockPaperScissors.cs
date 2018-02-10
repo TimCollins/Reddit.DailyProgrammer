def832e907cae9f2a7b89d5f9ba8b47f64e2055a
Tim <tim@timcollins.ie>
2018-02-10 15:01:16 +0000

    Calculate and display some statistics.

--- src/13-Hard-RPS/RockPaperScissors.cs
+++ src/13-Hard-RPS/RockPaperScissors.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _13_Hard_RPS
 {
@@ -6,11 +7,24 @@ namespace _13_Hard_RPS
     {
         private readonly int _maxGames;
         private readonly Random _rand;
+        private int _draws;
+        private int _p1Wins;
+        private int _p2Wins;
+
 
         public RockPaperScissors()
         {
             _maxGames = 100;
             _rand = new Random();
+
+            InitialiseProperties();
+        }
+
+        private void InitialiseProperties()
+        {
+            _draws = 0;
+            _p1Wins = 0;
+            _p2Wins = 0;
         }
 
         /// <summary>
@@ -21,6 +35,8 @@ namespace _13_Hard_RPS
         {
             _maxGames = maxGames;
             _rand = new Random();
+
+            InitialiseProperties();
         }
 
         public void Play()
@@ -32,10 +48,47 @@ namespace _13_Hard_RPS
                 var p1Guess = Choose();
                 var p2Guess = Choose();
                 var result = Evaluate(p1Guess, p2Guess);
+                UpdateStats(result);
                 var resultStr = Parse(result);
 
                 Console.WriteLine("P1: {0}\tP2: {1}\tResult: {2}", p1Guess, p2Guess, resultStr);
                 counter++;
+            }
+
+            DisplayStats();
+        }
+
+        private void DisplayStats()
+        {
+            var output = new StringBuilder();
+            var drawPercentage = (double) _draws / _maxGames * 100;
+            var p1Percentage = (double) _p1Wins / _maxGames * 100;
+            var p2Percentage = (double) _p2Wins / _maxGames * 100;
+
+            output.Append("Rock Paper Scissors Statistics\n");
+            output.Append(string.Format("Total games played: {0}\n", _maxGames));
+            output.Append(string.Format("Player 1 wins: {0} ({1})%\n", _p1Wins, p1Percentage));
+            output.Append(string.Format("Player 2 wins: {0} ({1})%\n", _p2Wins, p2Percentage));
+            output.Append(string.Format("Draws: {0} ({1})%", _draws, drawPercentage));
+
+            Console.WriteLine(output.ToString());
+        }
+
+        private void UpdateStats(string result)
+        {
+            if (result == "draw")
+            {
+                _draws++;
+            }
+
+            if (result == "p1")
+            {
+                _p1Wins++;
+            }
+
+            if (result == "p2")
+            {
+                _p2Wins++;
             }
         }
 

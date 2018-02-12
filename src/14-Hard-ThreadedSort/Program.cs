c214cf4febb9995672272dcb44ca51a1e177a3f0
Tim <tim@timcollins.ie>
2018-02-12 18:24:17 +0000

    Initial commit.

---
+++ src/14-Hard-ThreadedSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using Util;
+
+namespace _14_Hard_ThreadedSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that will generate a random array / collection of 
+            // 1 million integers, then sort them using a multi-threaded algorithm.
+            //Your program should take the number of threads through standard input.
+            //Bonus points if you can find the most efficient number of threads for your program.
+
+            // Also look for the max number
+            var ts = new ThreadedSort();
+            ts.Generate();
+            ts.Sort();
+            ts.Display();
+
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/14-Hard-ThreadedSort/ThreadedSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _14_Hard_ThreadedSort
+{
+    public class ThreadedSort
+    {
+        //private const int SizeLimit = 1000000;
+        private const int SizeLimit = 100;
+        private const int NumberLimit = 100;
+        private int[] _numbers;
+        public void Generate()
+        {
+            _numbers = new int[SizeLimit];
+            var r = new Random();
+
+            for (var i = 0; i < SizeLimit; i++)
+            {
+                _numbers[i] = r.Next(1, NumberLimit);
+            }
+        }
+
+        public void Sort()
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        public void Display()
+        {
+            for (var i = 0; i < SizeLimit; i++)
+            {
+                Console.WriteLine("Index {0} - Number {1}", i, _numbers[i]);
+            }
+        }
+    }
+}

89dbf47c47ccc1887651b0858b5e3fe834a62034
Tim <tim@timcollins.ie>
2018-09-30 16:11:09 +0100

    Added Sieve implementation

---
+++ src/14-Int-Sieve/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Util;
+
+namespace _14_Int_Sieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int max = 100;
+            var calculator = new Sieve();
+
+            Console.WriteLine(calculator.FormatPrimes(calculator.CalculatePrimes(max)));
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
---
+++ src/14-Int-Sieve/Sieve.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace _14_Int_Sieve
+{
+    /// <summary>
+    /// See https://en.wikipedia.org/wiki/Sieve_of_Sundaram for definition
+    /// </summary>
+    public class Sieve
+    {
+        public int[] CalculatePrimes(int upperBound)
+        {
+            if (upperBound < 1)
+            {
+                return new int[0];
+            }
+
+            var primeFlags = new bool[upperBound];
+
+            for (var i = 2; i < upperBound; i++)
+            {
+                primeFlags[i] = true;
+            }
+
+            for (var i = 2; i < upperBound; i++)
+            {
+                if (primeFlags[i])
+                {
+                    for (var p = 2; (p * i) < upperBound; p++)
+                    {
+                        primeFlags[p * i] = false;
+                    }
+                }
+            }
+
+            var primes = new List<int>();
+
+            for (var i = 0; i < primeFlags.Length; i++)
+            {
+                if (primeFlags[i])
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes.ToArray();
+        }
+
+        public string FormatPrimes(int[] primes)
+        {
+            return string.Join(", ", primes);
+        }
+    }
+}

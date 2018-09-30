0b4fe1265ea71655a71cef1bda7e9177700d88e7
Tim <tim@timcollins.ie>
2018-09-30 16:17:56 +0100

    Added initial project files.

---
+++ src/14-Easy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace _14_Easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.WaitForEscape();
+        }
+    }
+}
